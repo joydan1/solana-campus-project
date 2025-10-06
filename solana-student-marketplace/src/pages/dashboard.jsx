ca1adbe363d37b8a64f307c31475268f218ff7b0
joy akpan <114065663+joydan1@users.noreply.github.com>
2025-10-06 17:32:39 +0100

    Update dashboard.jsx

--- solana-student-marketplace/src/pages/dashboard.jsx
+++ solana-student-marketplace/src/pages/dashboard.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { supabase } from "../lib/supabaseClient";
 import { useNavigate } from "react-router-dom";
-import Transactions from "../components/Transactions.jsx";
+import Transactions from "../pages/Transactions.jsx";
 
 
 export default function Dashboard() {
