ef648d94ed78c09c27d4a49070f083d3420c82b1
joy akpan <114065663+joydan1@users.noreply.github.com>
2025-10-11 14:14:50 +0100

    Update main.jsx

--- solana-student-marketplace/src/main.jsx
+++ solana-student-marketplace/src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./App.css";
-import Login from "./components/login.jsx";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
